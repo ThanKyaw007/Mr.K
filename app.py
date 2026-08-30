342ca73343fb642c7975f2e7705f6626e6caecd1
ThanKyaw007 <86512235+ThanKyaw007@users.noreply.github.com>
2026-08-30 11:04:09 +0630

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ OPENROUTER_API_KEY = "sk-or-v1-08f58599da23753c83d2163c5580063c4be6f21937e792d7e
 
 # ====== OpenRouter Settings ======
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-MODEL = "google/gemma-3-27b-it:free"
+MODEL = "openrouter/free"
 
 # ====== Flask ======
 flask_app = Flask(__name__)
