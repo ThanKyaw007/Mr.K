38217d2f0e4ac2deeac386d874e173e40315cfff
ThanKyaw007 <86512235+ThanKyaw007@users.noreply.github.com>
2026-08-31 15:37:19 +0630

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 # ====== Groq Settings ======
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
-MODEL = "llama-3.3-70b-versatile"
+MODEL = "deepseek-r1-distill-llama-70b"
 
 # ====== Flask ======
 flask_app = Flask(__name__)
